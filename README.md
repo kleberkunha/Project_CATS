fd5039d4a7cca5bb0c18d2c038201a5d59c6c278
cmarion-mq <cmarion.mq@gmail.com>
2021-06-07 17:09:43 +0200

    create_db_and_seed

--- README.md
+++ README.md
@@ -26,3 +26,14 @@ Things you may want to cover:
 Branchement dev Caro
 
 first push
+
+Création de l'admin du site : un utilisateur de password "Anonymous", qui a pour email "anonymous@yopmail.com".
+
+Génération plus aléatoire :
+
+Le password du 2-ième utilisateur - de prénom "Adan", de nom "Shields" et d'email "adan_shields@yopmail.com" - créé par ce seed sera : "Jx7PtRg0".
+
+Le password du 3-ième utilisateur - de prénom "Domenic", de nom "Nolan" et d'email "domenic_nolan@yopmail.com" - créé par ce seed sera : "DbIu9A".
+
+Le password du 4-ième utilisateur - de prénom "Deangelo", de nom "Mckenzie" et d'email "deangelo_mckenzie@yopmail.com" - créé par ce seed sera : "E06nEeL".
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,77 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'bcrypt'
+
+ActiveRecord::Base.connection.tables.each do |table|
+  if table != "ar_internal_metadata" && table != "schema_migrations"
+    puts "Resetting auto increment ID for #{table} to 1"
+    ActiveRecord::Base.connection.execute("ALTER SEQUENCE #{table}_id_seq RESTART WITH 1")
+  end
+end
+Order.destroy_all
+Cart.destroy_all
+Item.destroy_all
+User.destroy_all
+
+users_array = []
+items_array = []
+carts_array = []
+orders_array = []
+
+Faker::Config.locale = 'en-US'
+
+puts "\nCréation de l'admin du site : un utilisateur de password \"Anonymous\", qui a pour email \"anonymous@yopmail.com\"."
+
+anonymous_user = User.new(
+email: "anonymous@yopmail.com",
+                          encrypted_password: BCrypt::Password.create("Anonymous")
+                         )
+anonymous_user.save!(:validate => false)
+users_array << anonymous_user
+
+puts "\nGénération plus aléatoire :"
+
+n = 3
+
+n.times do |index|
+  first_name = Faker::Name.unique.first_name
+  last_name = Faker::Name.unique.last_name
+  email = "#{first_name.gsub(' ', '').downcase}_#{last_name.gsub(' ', '').downcase}@yopmail.com"
+  password = Faker::Internet.password(min_length: 6, max_length: 10)
+  puts "\nLe password du #{index + 2}-ième utilisateur - de prénom \"#{first_name.capitalize}\", de nom \"#{last_name.capitalize}\" et d'email \"#{email}\" - créé par ce seed sera : \"#{password}\"."
+  user = User.new(email: "#{email}",
+                  encrypted_password: BCrypt::Password.create(password)
+                 )
+  user.save!(:validate => false)
+  users_array << user
+
+  item = Item.create( title: "Photo n°#{index + 1}",
+                      description: "Photo de chaton(s) fictive créée par seed.",
+                      price: Faker::Number.between(from: 1.0, to: 1000.0),
+                      image_url: Faker::Internet.url
+                    )
+  items_array << item
+
+  order = Order.new()
+  order.user_id = users_array[index+1].id
+  order.item_id = items_array[index].id
+  order.save
+
+  orders_array << order
+end
+
+require 'table_print'
+
+tp.set User, :id, :email, :encrypted_password
+puts "#{users_array.length} users créés :\n"
+tp User.all
+
+tp.set Item, :id,
+                {title: {:width => 13}},
+                {description: {:width => 70}},
+                {price: {:width => 5}},
+                {image_url: {:width => 170}}
+puts "\n#{items_array.length} photos créées :\n"
+tp Item.all
+
+puts "\n#{orders_array.length} orders créées :\n"
+tp Order.all
