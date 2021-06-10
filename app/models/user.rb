94f21904e4b690a45d5077060d90b2add273b59b
kleberkunha <kleberkunha@hotmail.com>
2021-06-10 17:08:51 +0200

    production teste

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :orders
   has_one :cart
 
-  after_create :welcome_send
+  #after_create :welcome_send
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
