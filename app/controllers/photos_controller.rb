473d2816a5eca0a0ac6232f5b7f86928799caca4
cmarion-mq <cmarion.mq@gmail.com>
2021-06-11 07:49:08 +0200

    active storage modif

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,8 @@
 class PhotosController < ApplicationController
   def create
-    @item = Item.find(params[:id])
-    @item.photo.attach(params[:avatar])
+    @item = Item.find(params[:item_id])
+    @item.photo.attach(params[:photo])
     flash[:success] = "You successfully added your photo"
-    redirect_to(user_path(@item))
+    redirect_to(root_path)
   end
 end
